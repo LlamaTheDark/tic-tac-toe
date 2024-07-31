978383b23d944c93e27a25bdaddb8790225b4954
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-31 15:04:45 -0600

    init: IOUtility

--- src/main/java/tictactoe/util/IOUtility.java
+++ src/main/java/tictactoe/util/IOUtility.java
@@ -1,5 +1,13 @@
 package tictactoe.util;
 
+import java.util.Scanner;
+
 public
 class IOUtility {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public
+    String getNextWord() {
+        return scanner.next();
+    }
 }
